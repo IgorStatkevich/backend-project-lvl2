c3c9784dfda3c12774c0a52271e3bbefa5dd4e69
Igor Statkevich <igorblood906@gmail.com>
2021-02-15 17:06:26 +0300

    Add test features

--- Makefile
+++ Makefile
@@ -5,4 +5,7 @@ publish:
 		npm publish --dry-run
 
 lint:
-	npx eslint ..
+	npx eslint .
+
+test:
+	npm test
---
+++ __tests__/index.test.js
@@ -0,0 +1,20 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import genDiff from '../index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const result = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+test('two JSON files', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(result);
+});
