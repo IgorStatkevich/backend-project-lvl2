ea20f8b88148e64ae26a4e1ed2b5008562ec2c8f
Igor Statkevich <igorblood906@gmail.com>
2021-02-16 09:06:58 +0300

    Add more index.js tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,7 +6,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const result = `{
+
+test('two JSON files', () => {
+  const result = `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -14,7 +16,23 @@ const result = `{
   + timeout: 20
   + verbose: true
 }`;
-
-test('two JSON files', () => {
   expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(result);
+});
+
+test('empty source, non-empty target', () => {
+  const result = `{
+  + host: hexlet.io
+  + timeout: 20
+  + verbose: true
+}`;
+  expect(genDiff(getFixturePath('empty.json'), getFixturePath('file2.json'))).toEqual(result);
+});
+
+test('non-empty source, empty target', () => {
+  const result = `{
+  - host: hexlet.io
+  - timeout: 20
+  - verbose: true
+}`;
+  expect(genDiff(getFixturePath('file2.json'), getFixturePath('empty.json'))).toEqual(result);
 });
